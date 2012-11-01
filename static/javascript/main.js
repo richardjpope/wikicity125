7ca225ebf107f285ce32c6dc11ff183e9d68b519
Richard Pope <richardpope@Richards-MacBook-Air.local>
2012-11-01 20:53:25 +0000

    opac

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -16,8 +16,8 @@ function randomNumber(max){
 }
 
 function fakeLocation(){
-    lats = [51.656, 51.613, 51.510, 51.406]
-    lngs = [-0.376, -0.261, -0.135, -0.279]
+    lats = [51.656, 51.613, 51.510, 51.406];
+    lngs = [-0.376, -0.261, -0.135, -0.279];
     random = randomNumber(4);
     fake_location = {'coords':{'latitude': lats[random], 'longitude': lngs[random]}}
     handleGetCurrentPosition(fake_location)
@@ -32,11 +32,13 @@ function getLocation(){
 function handleGetCurrentPosition(location){
     url = '/articles/' + location.coords.latitude + '/'+ location.coords.longitude
     $.getJSON(url, function(data) { 
-        for (var i=0; i < data['articles'].length; i++) {
-            if($('#' + data['articles'][i]['id']).length == 0){
-                $('#results').append('<li id=' + data['articles'][i]['id']  + '><a data-transition="slideup" data-rel="dialog" href="/browser/' + data['articles'][i]['id'] + '">' +  data['articles'][i]['title'] + '</a></li>')
-            }
-        };
+        if(data['articles']){
+            for (var i=0; i < data['articles'].length; i++) {
+                if($('#' + data['articles'][i]['id']).length == 0){
+                    $('#results').prepend('<li id=' + data['articles'][i]['id']  + '><a data-transition="slideup" data-rel="dialog" href="/browser/' + data['articles'][i]['id'] + '">' +  data['articles'][i]['title'] + '</a></li>')
+                }
+            };
+        }
         refresh();
         if (fake == false){
             setTimeout(function(){getLocation()}, 5000);
@@ -47,8 +49,29 @@ function handleGetCurrentPosition(location){
     );
 }
 
+function opac(){
+    opacity = 1;
+    counter = 1
+    $('#results li').each(function(index) {
+        if(counter > 2 && counter % 3 === 0 ){
+            opacity = (opacity - 0.2);
+        }
+        
+        $(this).css({ opacity: opacity });
+        counter ++
+    });
+}
+
+function cleanList(){
+    while($('#results li').length  > 12){
+        $('#results li').last().remove();
+    }
+}
+
 function refresh(){    
     $('#results').listview('refresh');    
+    cleanList();
+    opac();
 }
 
 function onError(){
