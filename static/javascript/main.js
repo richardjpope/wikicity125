e7cea9eb5c5fa55b37bdeffc23e8d73b5435d8e0
Richard <richard@memespring.co.uk>
2012-11-17 13:05:45 +0000

    stuff

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,15 +1,34 @@
 var fake_route = [[51.52432,-0.10967],[51.52335,-0.10848],[51.52246,-0.10685],[51.52145,-0.10574],[51.52001,-0.10552],[51.51894,-0.10531],[51.51758,-0.10509],[51.51584,-0.10488],[51.51451,-0.10437],[51.51317,-0.10411],[51.51146,-0.10406],[51.50924,-0.10359],[51.50716,-0.10355],[51.50585,-0.10316],[51.50465,-0.10239],[51.50307,-0.10192],[51.5015,-0.10123],[51.49995,-0.1005],[51.49843,-0.0996],[51.49685,-0.0987],[51.49562,-0.0984],[51.4945,-0.09844],[51.4933,-0.09891],[51.49266,-0.09913],[51.49177,-0.09926],[51.49094,-0.09917],[51.49033,-0.0987],[51.4895,-0.09836],[51.48878,-0.09788],[51.48817,-0.09737],[51.48742,-0.09681],[51.48632,-0.09621],[51.48536,-0.09608],[51.4844,-0.09574],[51.48293,-0.09557],[51.4817,-0.09557],[51.48018,-0.09561],[51.47881,-0.09548],[51.47777,-0.09544],[51.47673,-0.09552],[51.47558,-0.09587],[51.47424,-0.09677],[51.47315,-0.0981],[51.47189,-0.09999],[51.47085,-0.10132],[51.46975,-0.10218],[51.46849,-0.10222],[51.46697,-0.10226],[51.46499,-0.10196],[51.46349,-0.10192],[51.46168,-0.10213],[51.45999,-0.10291],[51.45807,-0.10372],[51.45603,-0.10389],[51.45408,-0.10303],[51.45285,-0.10209],[51.4517,-0.10072],[51.45055,-0.09934],[51.44978,-0.10016],[51.44833,-0.10136],[51.44665,-0.10308],[51.44496,-0.10462],[51.4433,-0.10419],[51.44165,-0.10372]]
 var fake_count = 0
+var timeouts = []
 
-$('[data-role="page"]').live('pageshow', function () {    
-    //init
-    if (mode == 'run'){
-        getLocation();        
-    }else{
-        fakeLocation();        
+$(document).delegate('.ui-page', 'pageshow', function () {
+
+    //reset
+    $('#results').html('')
+    fake_count = 0
+    for (var i=0; i < timeouts.length; i++) {
+        clearTimeout(timeouts[i]);
+    };
+
+    //go
+    if (get_mode() == 'run'){
+        getLocation();
+    }else if (get_mode() == 'example'){
+        fakeLocation();
     }
 });
 
+function get_mode(){
+  result = 'none';
+  if (window.location.search.indexOf('mode=run') > -1){
+    result = 'run';
+  } else if (window.location.search.indexOf('mode=example') > -1){
+      result = 'example';
+  }
+
+  return result;
+}
 function randomNumber(max){
     return Math.floor(Math.random()* (max + 1))
 }
@@ -29,7 +48,7 @@ function getLocation(){
 }
 
 function handleGetCurrentPosition(location){
-    url = '/articles/' + location.coords.latitude + '/'+ location.coords.longitude + '?mode=' + mode
+    url = '/articles/' + location.coords.latitude + '/'+ location.coords.longitude + '?mode=' + get_mode()
     $.getJSON(url, function(data) { 
         if(data['articles']){
             for (var i=0; i < data['articles'].length; i++) {
@@ -39,10 +58,10 @@ function handleGetCurrentPosition(location){
             };
         }
         refresh();
-        if (mode == 'run'){
-            setTimeout(function(){getLocation()}, 5000);
-        }else if (mode == 'example'){
-            setTimeout(function(){fakeLocation()}, 5000);
+        if (get_mode() == 'run'){
+            timeouts.push(setTimeout(function(){getLocation();}, 5000));
+        }else if (get_mode() == 'example'){
+            timeouts.push(setTimeout(function(){fakeLocation();}, 8000));
         }
       }
     );
@@ -67,7 +86,7 @@ function opac(){
         if(counter > 3 && counter % 4 === 0 ){
             opacity = (opacity - 0.2);
         }
-        
+
         $(this).css({ opacity: opacity });
         counter ++
     });
